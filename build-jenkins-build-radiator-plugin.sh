77f9b01566aab11c64ad3431c65119360908d92b
Petri Sirkkala <sirpete@iki.fi>
2015-01-23 23:14:15 +0200

    Unfortunately output redirect was eating the container id too

--- build-jenkins-build-radiator-plugin.sh
+++ build-jenkins-build-radiator-plugin.sh
@@ -2,13 +2,16 @@
 read -r -d '' BUILD_SCRIPT <<- EndOfBuildScript
 git clone https://github.com/jan-molak/jenkins-build-monitor-plugin.git
 cd jenkins-build-monitor-plugin
-mvn package
+#mvn package
+mkdir -p target
 EndOfBuildScript
 
-id=$(docker run -i sirkkalap/jenkins-swarm-slave-nlm /bin/bash -c 1>&2 "$BUILD_SCRIPT")
+echo Running build. Please wait. To see the output please run docker logs -f <id>
+
+id=$(docker run -i sirkkalap/jenkins-swarm-slave-nlm /bin/bash -c "$BUILD_SCRIPT")
 
 mkdir -p jenkins-build-monitor-plugin
 docker cp $id:/home/jenkins-slave/jenkins-build-monitor-plugin/target jenkins-build-monitor-plugin
-
-docker rm -f $id
+docker stop $id
+docker rm $id
 
