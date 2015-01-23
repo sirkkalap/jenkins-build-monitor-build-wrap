e7fd1b535c325cad582346ae9636731e65c7616e
Petri Sirkkala <sirpete@iki.fi>
2015-01-23 23:21:07 +0200

    Another approach to see the output

--- build-jenkins-build-radiator-plugin.sh
+++ build-jenkins-build-radiator-plugin.sh
@@ -2,16 +2,15 @@
 read -r -d '' BUILD_SCRIPT <<- EndOfBuildScript
 git clone https://github.com/jan-molak/jenkins-build-monitor-plugin.git
 cd jenkins-build-monitor-plugin
-#mvn package
-mkdir -p target
+mvn package
 EndOfBuildScript
 
-echo Running build. Please wait. To see the output please run docker logs -f <id>
+echo Running build. Please wait.
 
-id=$(docker run -i sirkkalap/jenkins-swarm-slave-nlm /bin/bash -c "$BUILD_SCRIPT")
+docker run -i --name build-job sirkkalap/jenkins-swarm-slave-nlm /bin/bash -c "$BUILD_SCRIPT"
 
 mkdir -p jenkins-build-monitor-plugin
-docker cp $id:/home/jenkins-slave/jenkins-build-monitor-plugin/target jenkins-build-monitor-plugin
-docker stop $id
-docker rm $id
+docker cp build-job:/home/jenkins-slave/jenkins-build-monitor-plugin/target jenkins-build-monitor-plugin
+docker stop build-job
+docker rm build-job
 
