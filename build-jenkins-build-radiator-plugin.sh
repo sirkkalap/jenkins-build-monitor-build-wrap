5a234dfbd29c2bc00141af4155142c0718635f26
Petri Sirkkala <sirpete@iki.fi>
2015-01-23 12:25:18 +0200

    It compiles...

---
+++ build-jenkins-build-radiator-plugin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+read -r -d '' BUILD_SCRIPT <<- EndOfBuildScript
+git clone https://github.com/jan-molak/jenkins-build-monitor-plugin.git
+cd jenkins-build-monitor-plugin
+mvn package
+EndOfBuildScript
+
+id=$(docker run -it sirkkalap/jenkins-swarm-slave-nlm /bin/bash -c 1>&2 "$BUILD_SCRIPT")
+
+docker cp $id:/home/jenkins-slave/jenkins-build-monitor-plugin/target jenkins-build-monitor-plugin
+
+docker rm $id
+
